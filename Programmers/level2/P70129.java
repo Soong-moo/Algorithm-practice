810ea74a625081928d3aa5f0f9fd8c5e5ce41762
Soong-moo <blueday0214@gmail.com>
2022-10-08 23:40:10 +0900

    add : level2/P70129

---
+++ Programmers/level2/P70129.java
@@ -0,0 +1,31 @@
+package level2;
+
+public class P70129 {
+	// /https://parkjye.tistory.com/81
+
+	class Solution {
+	    public int[] solution(String s) {
+	        
+	        int cnt = 0; //1이 될 때까지 변환 횟수
+	        int zero = 0; //제거한 0의 개수
+
+	        while(s.length() > 1) {
+	        
+	            int one = 0; // 1의 개수
+	            
+	            for(int i = 0; i < s.length(); i++) {
+	                if(s.charAt(i) == '0') zero++;
+	                else one++;
+	            
+	            }
+	            s = Integer.toBinaryString(one);
+	            cnt++;
+	        }
+	        
+	        int[] answer = {cnt,zero};
+	        
+	        return answer;
+	    }
+	}
+	
+}
