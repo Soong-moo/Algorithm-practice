9bf458092b0a7ce8c8fbfb1b17dfaaadd22b7ed3
Soong-moo <blueday0214@gmail.com>
2023-03-02 14:11:07 +0900

    add : level2/P131701, 연속 부분 수열 합의 개수

---
+++ Programmers/level2/P131701.java
@@ -0,0 +1,31 @@
+//https://hstory0208.tistory.com/166
+package level2;
+
+import java.util.HashSet;
+
+public class P131701 {
+
+	class Solution {
+		
+	    public int solution(int[] elements) {
+	        
+	        HashSet<Integer> hs = new HashSet<>();
+	        
+	        int start = 1;
+	        
+	        while(start <= elements.length) { 
+	            for(int i = 0; i < elements.length; i++) {
+	                int value = 0;
+	                for(int j = i; j < i + start; j++) {
+	                    value += elements[j % elements.length];
+	                }
+	                hs.add(value);
+	            }
+	            start++;  
+	        }
+	        
+	        return hs.size();
+	    }
+	}
+	
+}
