4cc62de69281e35d2430e537b76e3213110fde54
Soong-moo <blueday0214@gmail.com>
2023-04-18 17:34:32 +0900

    add : P10798, 세로읽기

---
+++ Baekjoon/JAVA/P10798.java
@@ -0,0 +1,30 @@
+package baekjoon_intelliJ;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class P10798 {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        char[][] word = new char[5][15];
+
+        for(int i = 0; i < 5; i++) {
+            String str = br.readLine();
+
+            for(int j = 0; j < str.length(); j++) {
+                word[i][j] = str.charAt(j);
+            }
+        }
+
+        for(int i = 0; i < 15; i++) {
+            for(int j = 0; j < 5; j++) {
+                if(word[j][i] != '\0') sb.append(word[j][i]);
+            }
+        }
+
+        System.out.println(sb.toString());
+    }
+}
