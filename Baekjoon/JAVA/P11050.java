85dc64972e3eab2bd263041c2305adf579259c7b
Soong-moo <blueday0214@gmail.com>
2023-05-25 12:21:42 +0900

    add : P11050, 이항 계수 1

---
+++ Baekjoon/JAVA/P11050.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P11050 {
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int answer = 1;
+
+        for(int i = 0; i < k; i++) {
+            answer *= n;
+            n--;
+        }
+
+        for(int i = 1; i <= k; i++) {
+            answer /= i;
+        }
+
+        System.out.println(answer);
+    }
+}
