d80317e066bcd5515e76e1661ab338cef1e7d5ac
Soong-moo <blueday0214@gmail.com>
2024-09-07 17:11:08 +0900

    add : P10989, 수 정렬하기 3

---
+++ Baekjoon/JAVA/P10989.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class P10989 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] array = new int[N];
+
+        for(int i = 0; i < N; i++) {
+            array[i] = Integer.parseInt(br.readLine());
+        }
+
+        br.close();
+
+        Arrays.sort(array);
+
+        for(int i = 0; i < N; i++) {
+            bw.write(array[i] + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
