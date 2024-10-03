0441faf71d4f1b6c2a839951104f55ce63e42cae
Soong-moo <blueday0214@gmail.com>
2024-10-03 15:19:21 +0900

    add : P11866, 요세푸스 문제 0

---
+++ Baekjoon/JAVA/P11866.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class P11866 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("<");
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        for(int i = 1; i <= N; i++) {
+            queue.add(i);
+        }
+
+        while(queue.size() > 1) {
+            for(int i = 1; i < K; i++) {
+                queue.offer(queue.poll());
+            }
+
+            sb.append(queue.poll()).append(", ");
+        }
+
+        sb.append(queue.poll()).append(">");
+
+        System.out.println(sb);
+
+    }
+}
